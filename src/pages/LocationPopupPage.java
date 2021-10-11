1214f002eefbbf25d37ac94254beb2b4cf8d1c11
Zavisa Sauer <zavisa.sauer@gmail.com>
2021-10-11 21:15:16 +0200

    Modified name of class LocationPopupPage

---
+++ src/pages/LocationPopupPage.java
@@ -0,0 +1,42 @@
+package pages;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LocationPopupPage extends BasicPage {
+
+	public LocationPopupPage(WebDriver driver, JavascriptExecutor js, WebDriverWait waiter) {
+		super(driver, js, waiter);
+
+	}
+
+	public WebElement getLocationHeader() {
+
+	}
+
+	public WebElement getCloseElement() {
+
+	}
+	
+	public void setLocation() {
+		
+	}
+
+	public WebElement getKeyword() {
+		
+	}
+
+	public WebElement getLocationInput() {
+
+	}
+
+	public WebElement getSubmit() {
+
+	}
+
+	public WebElement openDialog() {
+
+	}
+}
