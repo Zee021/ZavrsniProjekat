420c60f23e0fd0ddc9cc4663f4e7bd2bb34c8d4b
Zavisa Sauer <zavisa.sauer@gmail.com>
2021-10-12 21:52:26 +0200

    Updated AuthPage

--- src/pages/AuthPage.java
+++ src/pages/AuthPage.java
@@ -1,5 +1,32 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class AuthPage extends BasicPage{
 
+	public AuthPage(WebDriver driver, JavascriptExecutor js, WebDriverWait waiter) {
+		super(driver, js, waiter);
+	}
+	
+	public WebElement getAccDropdown() {
+		return this.driver.findElement(By.xpath("//div[@class='accounts-link accounts-popup']"));
+	}
+	
+	public WebElement getMyAccount() {
+		return this.driver.findElement(By.xpath("//div[@class='my-account-dropdown']/ul/li/a"));
+	}
+	public WebElement getLogout() {
+		return this.driver.findElement(By.xpath("//div[@class='my-account-dropdown']/ul/li[2]/a"));
+	}
+	
+	public void logout() {
+		this.getAccDropdown().click();
+		waiter.until(ExpectedConditions.visibilityOf(getLogout()));
+		this.getLogout().click();
+	}
 }
