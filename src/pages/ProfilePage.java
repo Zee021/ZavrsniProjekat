ae6f20af0e361ef76c7263c5fe00bcccca4af8b0
Zavisa Sauer <zavisa.sauer@gmail.com>
2021-10-12 20:27:27 +0200

    Updated ProfilePage

--- src/pages/ProfilePage.java
+++ src/pages/ProfilePage.java
@@ -1,5 +1,12 @@
 package pages;
 
-public class ProfilePage extends BasicPage{
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+public class ProfilePage extends BasicPage {
+
+	public ProfilePage(WebDriver driver, JavascriptExecutor js, WebDriverWait waiter) {
+		super(driver, js, waiter);
+	}
 }
