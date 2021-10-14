5a11be40e4736cccaaa40cbadc96245ae3026ece
Zavisa Sauer <zavisa.sauer@gmail.com>
2021-10-14 20:34:29 +0200

    Updated CartSummaryPage

--- src/pages/CartSummaryPage.java
+++ src/pages/CartSummaryPage.java
@@ -1,5 +1,22 @@
 package pages;
 
-public class CartSummaryPage extends BasicPage{
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+public class CartSummaryPage extends BasicPage {
+
+	public CartSummaryPage(WebDriver driver, JavascriptExecutor js, WebDriverWait waiter) {
+		super(driver, js, waiter);
+	}
+
+	public WebElement getClearAllButton() {
+		return this.driver.findElement(By.xpath("//a[@class='btn btn--third  btn--small no-radius']"));
+	}
+
+	public void clearAllinCart() {
+		this.getClearAllButton().click();
+	}
 }
