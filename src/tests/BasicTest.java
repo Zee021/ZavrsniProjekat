9fac173865ad275746f7f4d9cb377efbbeed4900
Zavisa Sauer <zavisa.sauer@gmail.com>
2021-10-15 00:03:53 +0200

    Updated BasicTest

--- src/tests/BasicTest.java
+++ src/tests/BasicTest.java
@@ -1,5 +1,92 @@
 package tests;
 
-public abstract class BasicTest {
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.asserts.SoftAssert;
+
+import pages.AuthPage;
+import pages.CartSummaryPage;
+import pages.LocationPopupPage;
+import pages.LoginPage;
+import pages.MealPage;
+import pages.NotificationSystemPage;
+import pages.ProfilePage;
+import pages.SearchResultPage;
+
+import org.openqa.selenium.TakesScreenshot;
+
+public abstract class BasicTest {
+	protected WebDriver driver;
+	protected JavascriptExecutor js;
+	protected WebDriverWait waiter;
+	protected SoftAssert sa;
+
+	protected String baseUrl;
+	protected String email;
+	protected String password;
+	protected String locationName;
+
+	protected LocationPopupPage popUpPage;
+	protected LoginPage loginPage;
+	protected NotificationSystemPage notificationPage;
+	protected ProfilePage profilePage;
+	protected AuthPage authPage;
+	protected MealPage mealPage;
+	protected CartSummaryPage summaryPage;
+	protected SearchResultPage searchPage;
+
+	@BeforeMethod
+	public void setUp() throws IOException {
+		System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
+		this.driver = new ChromeDriver();
+		this.waiter = new WebDriverWait(driver, 20);
+		this.js = (JavascriptExecutor) driver;
+
+		this.baseUrl = "http://demo.yo-meals.com/";
+		this.email = "customer@dummyid.com";
+		this.password = "12345678a";
+		this.locationName = "City Center - Albany";
+
+		this.popUpPage = new LocationPopupPage(driver, js, waiter);
+		this.loginPage = new LoginPage(driver, js, waiter);
+		this.profilePage = new ProfilePage(driver, js, waiter);
+		this.notificationPage = new NotificationSystemPage(driver, js, waiter);
+		this.authPage = new AuthPage(driver, js, waiter);
+		this.mealPage = new MealPage(driver, js, waiter);
+		this.summaryPage = new CartSummaryPage(driver, js, waiter);
+		this.searchPage = new SearchResultPage(driver, js, waiter);
+
+		this.driver.manage().window().maximize();
+		this.driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		this.driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+	}
+
+	@AfterMethod
+	public void afterTest(ITestResult result) throws IOException, InterruptedException {
+
+		if (result.getStatus() == ITestResult.FAILURE) {
+			File screenShot = ((TakesScreenshot) this.driver).getScreenshotAs(OutputType.FILE);
+			String fileName = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss'.png'").format(new Date());
+			File save = new File("screenshots/" + fileName);
+			FileHandler.copy(screenShot, save);
+			Thread.sleep(2000);
+			this.driver.quit();
+		} else {
+			this.driver.quit();
+		}
+	}
 }
